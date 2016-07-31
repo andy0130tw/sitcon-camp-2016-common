bbe565d8e4340c30badbb25e844f9018ab05a412
Andy Pan <andy0130tw@yahoo.com.tw>
2016-07-31 16:48:12 +0800

    fix tlkio and gen

--- gen/gen.js
+++ gen/gen.js
@@ -3,22 +3,36 @@
 var fs = require('fs');
 var minify = require('html-minifier').minify;
 
-var toc = require('./toc'); 
+var toc = require('./toc');
 
 var prologueTemplate = fs.readFileSync('./prologue.txt', 'utf-8');
 var epilogueTemplate = fs.readFileSync('./epilogue.txt', 'utf-8');
+
+var TLKIO_CSS_DATA_URI = 'data:text/css,' +
+  escape(escape(
+    'html{overflow-y:auto}' +
+    '#channel{font-size:18px}' +
+    '.theme--day{background-color:#f4f3f1!important}' +
+    '.theme--pop{background-color:#f9eced!important}' +
+    '.theme--minimal{background-color:white!important}' +
+    '.theme--night{background-color:#26282c!important}'
+  ))
+    .replace(/%2521/g, '!')
+    .replace(/%253A/g, ':')
+    .replace(/%257B/g, '{')
+    .replace(/%257D/g, '}');  // to save characters
 
 toc.forEach(function(tocEntry) {
   if (!tocEntry.path)
     throw new Error('Missing output path: ' + JSON.stringify(tocEntry));
   if (!tocEntry.slides)
     throw new Error('Missing slides: ' + JSON.stringify(tocEntry));
-    
+
   var prologue = prologueTemplate
     .replace('{{title}}', tocEntry.title || '')
     .replace('{{description}}', tocEntry.description || '')
     .replace('{{author}}', tocEntry.author || 'Kuang-Lin Pan');
-    
+
   var slides = tocEntry.slides.map(function(path) {
       return '<section\
           data-markdown="' + path + '"\
@@ -26,15 +40,16 @@ toc.forEach(function(tocEntry) {
           data-separator-vertical="^\\n---\\n$">\
         </section>';
   }).join('');
-  
-  var epilogue = epilogueTemplate;
-  
-  // always add \n to the end of the file 
+
+  var epilogue = epilogueTemplate
+    .replace('{{tlkio-css}}', TLKIO_CSS_DATA_URI);
+
+  // always add \n to the end of the file
   var minified = minify(prologue + slides + epilogue, {
       collapseWhitespace: true,
       minifyJS: true
   }) + '\n';
-  
+
   fs.writeFileSync('../' + tocEntry.path, minified, 'utf-8');
   console.log('Output:', tocEntry.path);
 });
